d0a931c33b09c556c3e3202b72d006c33551ece0
sh kwon <harvy1070@gmail.com>
2025-03-17 09:34:58 +0900

    ㅁㄹㄴㅇㄹ

--- harvy_be/harvy_portfolio/settings_prod.py
+++ harvy_be/harvy_portfolio/settings_prod.py
@@ -49,9 +49,14 @@ STATICFILES_DIRS = []
 if 'whitenoise.middleware.WhiteNoiseMiddleware' not in MIDDLEWARE:
     MIDDLEWARE = ['whitenoise.middleware.WhiteNoiseMiddleware'] + MIDDLEWARE
 
-CORS_ALLOW_ALL_ORIGINS = os.environ.get('CORS_ALLOW_ALL_ORIGINS', 'False') == 'True'
-# CORS_ALLOWED_ORIGINS = True
-CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', 'https://harvy13.netlify.app,http://harvy.kr,http://www.harvy.kr').split(',')
+# CORS_ALLOW_ALL_ORIGINS = os.environ.get('CORS_ALLOW_ALL_ORIGINS', 'False') == 'True'
+CORS_ALLOWED_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', 'https://harvy13.netlify.app,http://harvy.kr,http://www.harvy.kr').split(',')
+CORS_ALLOWED_ORIGINS = [
+    'https://harvy13.netlify.app',
+    'http://harvy.kr',
+    'http://www.harvy.kr'
+]
 
 # Security settings
 SECURE_SSL_REDIRECT = True
