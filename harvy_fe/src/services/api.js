b822cca2b6e0e052b2619fd6c9f7b83c1d04167e
sh kwon <harvy1070@gmail.com>
2025-03-17 09:45:44 +0900

    DNS HTTPS 세팅으로 변경

--- harvy_fe/src/services/api.js
+++ harvy_fe/src/services/api.js
@@ -3,9 +3,10 @@ import store from '@/store';
 
 // 환경에 따라 API 기본 URL 설정
 // const API_BASE_URL = process.env.VUE_APP_API_BASE_URL || 'https://harvy-dev-f064f0b3b0ee.herokuapp.com/api/';
+const API_BASE_URL = process.env.VUE_APP_API_BASE_URL || 'https://www.harvy.kr/api/';
 // 임시 세팅
-const API_BASE_URL =
-    process.env.VUE_APP_API_BASE_URL || window.location.protocol + '//' + window.location.host + '/api/';
+// const API_BASE_URL =
+//     process.env.VUE_APP_API_BASE_URL || window.location.protocol + '//' + window.location.host + '/api/';
 console.log('Current Environment:', process.env.NODE_ENV);
 console.log('API_BASE_URL:', API_BASE_URL);
 
